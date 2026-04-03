1313289e5a794635657f34fc4e57327dd5ba791f
Colin M West <cmw84331@uga.edu>
2026-04-03 16:37:05 -0400

    skeleton code done

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,41 @@
-public class BinarySearchTree {
-    
+public class BinarySearchTree<T extends Comparable<T>> {
+    private NodeType<T> head;
+    private boolean printDuplicates;
+
+    public BinarySearchTree() {
+        head = null;
+        printDuplicates = false;
+    } //constructor
+
+    public void setPrintDuplicates(boolean setting) {
+        this.printDuplicates = setting;
+    } //setPrintDuplicates
+
+    public void insert(T key) {
+
+    } //insert
+
+    public void delete(T key) {
+
+    } //delete
+
+    public boolean retrieve(T item) {
+        return false;
+    } //retrieve
+
+    public void inOrder() {
+
+    } //inOrder
+
+    public void getSingleParent(T item) {
+
+    }
+
+    public void getNumLeafNodes() {
+
+    } //getNumLeafNodes
+
+    public void getCousins(T item) {
+
+    } //getCousins
 }
--- src/BinarySearchTreeDriver.java
+++ src/BinarySearchTreeDriver.java
@@ -5,6 +5,55 @@ public class BinarySearchTreeDriver {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter list type (i - int, d - double, s - string): ");
         String listType = scanner.nextLine();
-        
-    }
-}
+        boolean typeSelected = false;
+        while (!typeSelected) {
+            if (listType.equals("i")) {
+                BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+                typeSelected = true;
+                runCompatible(scanner, tree, listType);
+            } else if (listType.equals("d")) {
+                BinarySearchTree<Double> tree = new BinarySearchTree<>();
+                typeSelected = true;
+                runCompatible(scanner, tree, listType);
+            } else if (listType.equals("s")) {
+                BinarySearchTree<String> tree = new BinarySearchTree<>();
+                typeSelected = true;
+                runCompatible(scanner, tree, listType);
+            } else {
+                System.out.print("Invalid list type. Please try again: ");
+                listType = scanner.nextLine();
+            } //if-else 
+        } //while type not selected
+    } //main
+
+    private static <T extends Comparable<T>> void runCompatible(Scanner scanner, BinarySearchTree<T> tree, String listType) {
+        System.out.println("Commands:\n(i) - Insert Item\n(d) - Delete Item\n(p) - Print Tree\n(r) - Retrieve Item\n(l) - Count Leaf Nodes\n(s) - Find Single Parents\n(c) - Find Cousins\n(q) - Quit Program"); 
+        String previous = "i";
+        String input = doCommandCycle(scanner, previous, tree, listType);
+        while (!input.equalsIgnoreCase("q")) {
+            previous = input;
+            input = doCommandCycle(scanner, previous, tree, listType);
+        } //while user hasn't quit
+    } //runCompatible
+
+    private static <T extends Comparable<T>> String doCommandCycle(Scanner scanner, String previous, BinarySearchTree<T> tree, String listType) {
+        if (!previous.equalsIgnoreCase("invalid")) {
+            System.out.print("Enter a command: ");
+        } //if
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("i")) {
+        } else if (input.equalsIgnoreCase("d")) {
+        } else if (input.equalsIgnoreCase("p")) {
+        } else if (input.equalsIgnoreCase("r")) {
+        } else if (input.equalsIgnoreCase("l")) {
+        } else if (input.equalsIgnoreCase("s")) {
+        } else if (input.equalsIgnoreCase("c")) {
+        } else if (input.equalsIgnoreCase("q")) {
+            System.out.println("Exiting the program...");
+        } else {
+            System.out.print("Invalid command. Please try again. ");
+            input = "invalid";
+        } //if-else
+        return input;
+    } //doCommandCycle
+    } //BinarySearchTreeDriver
